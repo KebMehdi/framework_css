dc75fcb6b7b12ed9efdfbc9245d52cb6336661f7
Mehdi <mehdi.kebaili@epitech.eu>
2020-03-11 12:17:23 +0100

    push du projet

---
+++ sass/boostrap.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.close').click(function(){
+        $('.alert-dismissible').hide();
+    })
+    $('.close2').click(function(){
+        $('.alert-dismissible2').hide();
+    })
+}) 
